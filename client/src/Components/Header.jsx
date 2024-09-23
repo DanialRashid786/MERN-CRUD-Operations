bd1840f28071f4bade9518ba636ad9b33c574ebe
Danial Rashid <sp20-bse-046@students.cuisahiwal.edu.pk>
2024-09-24 00:10:16 +0500

    add daisyUI in React

--- client/src/Components/Header.jsx
+++ client/src/Components/Header.jsx
@@ -1,21 +1,70 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
     <>
-    <div>
-
-    <header>
-      <nav>
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-      </nav>
-    </header>
-
-    </div>
+      <div>
+        <header>
+          <div className="navbar bg-base-100" data-theme="cupcake">
+            <div className="navbar-start">
+              <div className="dropdown">
+                <div
+                  tabIndex={0}
+                  role="button"
+                  className="btn btn-ghost lg:hidden"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-5 w-5"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M4 6h16M4 12h8m-8 6h16"
+                    />
+                  </svg>
+                </div>
+                <ul
+                  tabIndex={0}
+                  className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+                >
+                  <li>
+                    <Link to="/">Home</Link>
+                  </li>
+                  <li>
+                    <Link to="/about">About</Link>
+                  </li>
+                </ul>
+              </div>
+              {/* Use Link instead of <a> */}
+              <Link to="/" className="btn btn-ghost text-xl">
+                daisyUI
+              </Link>
+            </div>
+            <div className="navbar-center hidden lg:flex">
+              <ul className="menu menu-horizontal px-1">
+                <li>
+                  <Link to="/">Home</Link>
+                </li>
+                <li>
+                  <Link to="/about">About</Link>
+                </li>
+              </ul>
+            </div>
+            <div className="navbar-end">
+              {/* Use button for non-navigation action */}
+              <button className="btn">Button</button>
+            </div>
+          </div>
+        </header>
+      </div>
     </>
-  )
+  );
 }
 
-export default Header
+export default Header;
